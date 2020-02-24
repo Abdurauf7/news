8dbc0ec400c9cb4acd8eb84a700a1096293fee18
Abdurauf <abdurauf@Abduraufs-MacBook-Pro.local>
2020-02-24 09:53:27 +0500

    Updates

---
+++ precache-manifest.a3499acaa05a5602a3dd0a50f7730c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ed5ed21145ec93ae36b9a2f9259885",
+    "url": "/news/index.html"
+  },
+  {
+    "revision": "10d98a37b38245c75731",
+    "url": "/news/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "95b85cff116b6803cf47",
+    "url": "/news/static/css/main.13faa6c1.chunk.css"
+  },
+  {
+    "revision": "10d98a37b38245c75731",
+    "url": "/news/static/js/2.06c37d81.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/news/static/js/2.06c37d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b85cff116b6803cf47",
+    "url": "/news/static/js/main.5c772e46.chunk.js"
+  },
+  {
+    "revision": "2c4206adfa79eb977118",
+    "url": "/news/static/js/runtime-main.c788b845.js"
+  }
+]);
